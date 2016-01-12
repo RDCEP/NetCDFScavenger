b211e0644477a7ee54c08dfdc050b9dfdfc5fe02
montella <raffaele.montella@uniparthenope.it>
2016-01-12 00:30:04 +0000

    Added config file

--- netcdf_scraper/spiders/NetCDFSpider.py
+++ netcdf_scraper/spiders/NetCDFSpider.py
@@ -1,5 +1,6 @@
 import subprocess
 
+from os.path import expanduser
 from parallel_sync import wget
 from netCDF4 import Dataset
 import json, math, os
@@ -8,15 +9,16 @@ import urlparse
 import tempfile
 import datetime
 
-from scrapy.contrib.spiders import CrawlSpider, Rule
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import HtmlXPathSelector
-from scrapy.spider import BaseSpider
 from scrapy.http import Request, Response
 from netcdf_scraper.items import NetCDFScraperItem
 
 from pymongo import MongoClient
 from pymongo.errors import PyMongoError
+
+import ConfigParser
 
 class NetCDFSpider(CrawlSpider):
     name = 'netcdf'
@@ -33,12 +35,15 @@ class NetCDFSpider(CrawlSpider):
         Rule(SgmlLinkExtractor(allow=(), restrict_xpaths=('//a',)), callback="parse_items", follow= True),
     )
 
+    def __init__(self):
+        super(NetCDFSpider, self).__init__()
+        home = expanduser("~")
+        self.config = ConfigParser.ConfigParser()
+        self.config.read(home+"/.netcdf-scavenger/config.ini")
+        self.mongodb_url="mongodb://"+self.configSectionMap("mongodb")['host']+":"+self.configSectionMap("mongodb")['port']+"/"
+
 
     def parse_items(self, response):
-	mongodb_url='mongodb://54.208.184.225:27017/'
-        user="galaxy"
-        password="Galaxy2015!"
-
         # Create an item
         item = NetCDFScraperItem()
 
@@ -50,7 +55,7 @@ class NetCDFSpider(CrawlSpider):
         if response.url.endswith(".nc4") or response.url.endswith(".nc") or response.url.endswith("dods"):
             stored_item=None
             # Try to check if this url has been visided using mongodb
-            client = MongoClient(mongodb_url)
+            client = MongoClient(self.mongodb_url)
             if client.netcdf.authenticate(user, password) is True:
                 db = client.netcdf
                 items = db.items
@@ -223,6 +228,19 @@ class NetCDFSpider(CrawlSpider):
             }
         rootgrp.close()
         return feature
+
+    def configSectionMap(self,section):
+        dict1 = {}
+        options = self.config.options(section)
+        for option in options:
+            try:
+                dict1[option] = self.config.get(section, option)
+                if dict1[option] == -1:
+                    print("skip: %s" % option)
+            except:
+                print("exception on %s!" % option)
+                dict1[option] = None
+        return dict1
 
 if __name__ == "__main__":
 	netcdfSpider=NetCDFSpider()
